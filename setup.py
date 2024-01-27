59ea8f1774fd9ed2c76a1917c7ef261d7016380c
dilipkumarait <93387256+dilipkumarait@users.noreply.github.com>
2024-01-27 18:17:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = "dilip_test",
     version="0.1",
     description= "added this to just for testing",
-    url= "hwfio",
+    url= "https://github.com/dilipkumarait/dilip_test.git",
     packages=find_packages(),
     install_requires = ["pandas", "pytest"]
 )
